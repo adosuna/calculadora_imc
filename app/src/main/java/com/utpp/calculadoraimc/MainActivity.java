ccdda729e3dbd33ec03aa2713f37eae51de08b3f
adosuna <adrianosuna@utpp.edu.mx>
2022-09-22 16:16:19 -0700

    22/09/2022 -Agregar IDs -Crear variables de elementos y metodos

--- app/src/main/java/com/utpp/calculadoraimc/MainActivity.java
+++ app/src/main/java/com/utpp/calculadoraimc/MainActivity.java
@@ -3,6 +3,11 @@ package com.utpp.calculadoraimc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +17,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //R = resource o recurso
 
-        String alertaTexto = "Este es mi texto de la variable";
+       TextView textoResultado = findViewById(R.id.vista_texto_resultado);
 
-        Toast.makeText(this, alertaTexto,
-                Toast.LENGTH_LONG).show();
+       RadioButton botonHombre = findViewById(R.id.boton_radio_hombre);
+       RadioButton botonMujer = findViewById(R.id.boton_radio_mujer);
+
+       EditText edadEditarTexto = findViewById(R.id.editar_texto_edad);
+       EditText centimetrosEditarTexto = findViewById(R.id.editar_texto_centimetros);
+       EditText pesoEditarTexto = findViewById(R.id.editar_texto_peso);
+
+       Button botonCalcular = findViewById(R.id.boton_calcular);
+
+       botonCalcular.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+               Toast.makeText(MainActivity.this, "Orale, ya reacciona a botonazos",
+                       Toast.LENGTH_LONG).show();
+           }
+       });
+
+
+       textoResultado.setText("Orale, ya puedo actualizar mi textview - soy la mera onda");
     }
 }
